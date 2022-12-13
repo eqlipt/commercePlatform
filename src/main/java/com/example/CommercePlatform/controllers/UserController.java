d7a88e68834d23cf09ae35356e9be022771195b6
eqlipt <squish@list.ru>
2022-12-13 12:53:53 +0300

    code cleanup

--- src/main/java/com/example/CommercePlatform/controllers/UserController.java
+++ src/main/java/com/example/CommercePlatform/controllers/UserController.java
@@ -6,10 +6,7 @@ import com.example.CommercePlatform.util.PersonValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/user")
